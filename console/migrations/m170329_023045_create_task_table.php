90536c47c9921f70baf3317431319dfa42001aff
José Carlos López <jcl.isc@gmail.com>
2017-04-02 13:57:03 -0500

    long date

--- console/migrations/m170329_023045_create_task_table.php
+++ console/migrations/m170329_023045_create_task_table.php
@@ -18,7 +18,7 @@ class m170329_023045_create_task_table extends Migration
             'title' => $this->string(200)->notNull(),
             'description' => $this->string(500),
             'pomodoro_cycles' => $this->integer()->check('pomodoro_cycles >= 0'),
-            'due_date' => 'timestamptz',
+            'due_date' => $this->,
             'created_at' => 'timestamptz'
         ]);
         $this->addForeignKey('task_user_fk', 'task', 'user_id', 'user', 'id');
